574ec2ec014e4aad6a699bbd02e52894466d3882
Mikołaj Leonhardt <mikolaj.leonhardt@gmail.com>
2025-05-14 00:20:52 +0200

    Caesar Cipher

--- Ciphers/CaesarCipher.hs
+++ Ciphers/CaesarCipher.hs
@@ -1,8 +1,17 @@
 module Ciphers.CaesarCipher where
 
+import Data.List (elemIndex)
+import Data.Maybe (maybe)
 
-encrypt :: String -> String
-encrypt plaintext = plaintext
+encrypt :: (String, Int) ->  String -> String
+encrypt (alphabet, shift) plaintext = [
+        -- for each letter in the plaintext find its index in the alphabet and change it to (index + shift) % length alphabet, then map this letter to the letter in alphabet with modified index
+        -- if the letter is not present in the alphabet, leave it as it is
+        maybe letter
+        (\index -> alphabet !! ((index + shift) `mod` length alphabet))
+        (letter `elemIndex` alphabet) | letter <- plaintext]
 
-decrypt :: String -> String
-decrypt ciphertext = ciphertext
+
+decrypt :: (String, Int) -> String -> String
+-- same as encrypt, but the shift is done in the opposite direction
+decrypt (alphabet, shift) = encrypt (alphabet, -shift)
